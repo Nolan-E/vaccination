136a1a41e514b62cc8bf063cbda520d43e01da52
Nolan Eckert <nolan.eckert@gmail.com>
2021-05-22 16:01:06 -0600

    Start navbar router components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,33 @@
-import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Home from "./Home"
+import Data from "./Data"
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Router>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/data">Data</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/data">
+            <Data />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  )
 }
 
 export default NavBar
